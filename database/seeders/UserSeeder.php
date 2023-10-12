ae1a07b1562d86a42c86beb622fe86942b677c5a
fabio-alkimia <fabio@alkimiadigital.it>
2023-10-12 10:03:06 +0200

    - Aggiunti User e Role Reseller e Customer;

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // Superadmin
         $superadmin = User::create([
             'name' => 'Superadmin',
             'email' => 'admin@example.test',
@@ -23,5 +24,27 @@ class UserSeeder extends Seeder
         ]);
 
         $superadmin->assignRole(Role::findByName('superadmin'));
+
+        // Reseller
+        $reseller = User::create([
+            'name' => 'Reseller',
+            'email' => 'reseller@example.test',
+            'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        $reseller->assignRole(Role::findByName('reseller'));
+
+        // Customer
+        $customer = User::create([
+            'name' => 'Customer',
+            'email' => 'customer@example.test',
+            'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        $customer->assignRole(Role::findByName('customer'));
     }
 }
