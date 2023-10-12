58b745b0813506537b8ba4668f0aba8b256d4dc4
fabio-alkimia <fabio@alkimiadigital.it>
2023-10-12 09:20:56 +0200

    - Aggiunto RolePermissionSeeder.php; - Aggiunto ruoli base "superadmin", "reseller", "customer"; - Modifica UserSeeder.php con assegnazione ruolo al superadmin creato;

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -20,5 +21,7 @@ class UserSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now()
         ]);
+
+        $superadmin->assignRole(Role::findByName('superadmin'));
     }
 }
