54ad302a5c70e439a78a9150d995a443ab59a77d
alessandro-algomera <alessandro.visintini@algomera.it>
2023-10-16 10:25:47 +0200

    Creata Trait per aggiungere uno scope per filtrare i dati in base alla nazione

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use App\Traits\HasCountryScope;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
@@ -14,7 +15,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, HasCountryScope;
 
     const SUPERADMIN = 'superadmin';
     const RESELLER   = 'reseller';
