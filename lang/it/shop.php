5d3c8679d5c0227bd30736320034c5afac05aaec
valerio-m-alkimia <valerio.m@alkimiadigital.it>
2023-10-16 17:30:14 +0200

    creazione file lang. shop

---
+++ lang/it/shop.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'header' => 'Spedizioni in tutto il mondo in 4/7 giorni',
+    'navigation' => [
+        'home' => 'Home',
+        'shop' => 'Shop',
+        'about_us' => 'Chi siamo',
+        'journal' => 'Giornale GearxPro',
+        'assistance' => 'Assistenza'
+    ],
+    'jumbotron' => [
+        'title' => 'Il negozio che stavi Aspettando',
+        'description' => 'The quick brown fox jumps over a lazy dog is the most famous pangram in English that is the most short.',
+        'button' => 'Acquista ora'
+    ],
+    'carousel-top' => [
+        'title' => 'In which all the 26 letters'
+    ]
+];
