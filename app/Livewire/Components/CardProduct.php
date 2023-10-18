a103a90344b7069a4f1cf6b72938801a13ffefbe
valerio-m-alkimia <valerio.m@alkimiadigital.it>
2023-10-18 18:55:34 +0200

    aggiornamento componente cardProduct

--- app/Livewire/Components/CardProduct.php
+++ app/Livewire/Components/CardProduct.php
@@ -3,13 +3,21 @@
 namespace App\Livewire\Components;
 
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class CardProduct extends Component
 {
+    public $cardSmall = false;
+    public $image;
     public $name;
     public $description;
     public $availableColor;
     public $price;
+
+    #[On('filterOn')]
+    public function filterOn() {
+        $this->cardSmall = !$this->cardSmall;
+    }
 
     public function render()
     {
--- resources/views/livewire/components/card-product.blade.php
+++ resources/views/livewire/components/card-product.blade.php
@@ -1,8 +1,13 @@
-<div class="min-w-[594px] max-w-[594px] h-[727px] rounded-b-md overflow-hidden">
-    <div class="w-full h-[calc(100%-108px)] bg-white flex items-center justify-center overflow-hidden">
-        <img src="{{ Vite::asset('resources/images/SOXPro.png')}}" alt="">
+<div
+    @class([
+        'grow rounded-b-md overflow-hidden',
+        $cardSmall ? 'max-w-[434px] h-[561px]' : 'max-w-[594px] h-[727px]',
+    ])
+ >
+    <div class="h-[calc(100%-108px)] bg-white flex items-center justify-center overflow-hidden">
+        <img src="{{ Vite::asset('resources/images/'. $image)}}" alt="">
     </div>
-    <div class="bg-white h-[108px] w-full flex border border-white border-x border-x-white hover:border-y-white rounded-b-md transition-all duration-300 group">
+    <div class="bg-white h-[108px] flex border border-white border-x border-x-white hover:border-y-white rounded-b-md transition-all duration-300 group">
         <div class="h-full grow flex flex-col px-[20px] py-[16px] relative">
             <h3 class="z-10 text-[15px] font-semibold text-color-18181a leading-[19px]">{{ $name }}</h3>
             <p class="z-10 text-[12px] font-medium text-color-6c757d">{{ $description }}</p>
