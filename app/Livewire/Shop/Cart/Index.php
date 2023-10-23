1ff837c50d57fc85bb7268d244767101399302a9
valerio-m-alkimia <valerio.m@alkimiadigital.it>
2023-10-23 19:24:47 +0200

    wip cart.index

--- app/Livewire/Shop/Cart/Index.php
+++ app/Livewire/Shop/Cart/Index.php
@@ -5,10 +5,61 @@ namespace App\Livewire\Shop\Cart;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 
-
 #[Layout('layouts.guest')]
 class Index extends Component
 {
+    public $productsRecommended = [
+        0 => [
+            'name' => 'SoXPro',
+            'image' => 'SOXPro.png',
+            'description' => 'SOXPro Trekking - Green',
+            'availableColor' => '1',
+            'price' => '29,00 - € 35,00'
+        ],
+        1 => [
+            'name' => 'SoXPro',
+            'image' => 'SOXPro.png',
+            'description' => 'SOXPro Trekking - Green',
+            'availableColor' => '1',
+            'price' => '29,00 - € 35,00'
+        ],
+        2 => [
+            'name' => 'SoXPro',
+            'image' => 'SOXPro.png',
+            'description' => 'SOXPro Trekking - Green',
+            'availableColor' => '1',
+            'price' => '29,00 - € 35,00'
+        ],
+        3 => [
+            'name' => 'SoXPro',
+            'image' => 'SOXPro.png',
+            'description' => 'SOXPro Trekking - Green',
+            'availableColor' => '1',
+            'price' => '29,00 - € 35,00'
+        ],
+        4 => [
+            'name' => 'SoXPro',
+            'image' => 'SOXPro.png',
+            'description' => 'SOXPro Trekking - Green',
+            'availableColor' => '1',
+            'price' => '29,00 - € 35,00'
+        ],
+        5 => [
+            'name' => 'SoXPro',
+            'image' => 'SOXPro.png',
+            'description' => 'SOXPro Trekking - Green',
+            'availableColor' => '1',
+            'price' => '29,00 - € 35,00'
+        ],
+        6 => [
+            'name' => 'SoXPro',
+            'image' => 'SOXPro.png',
+            'description' => 'SOXPro Trekking - Green',
+            'availableColor' => '1',
+            'price' => '29,00 - € 35,00'
+        ],
+    ];
+
     public function render()
     {
         return view('livewire.shop.cart.index');
