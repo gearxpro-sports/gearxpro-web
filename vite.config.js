5c2a866be712586805f3cec8a800ccc730a35b88
fabio-alkimia <fabio@alkimiadigital.it>
2023-10-26 18:28:46 +0200

    - Aggiunto owl-carousel (css e js) come input in vite.config.js;

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,10 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/css/shop.css',
                 'resources/css/auth.css',
+                'resources/css/owl.carousel.css',
                 'resources/js/app.js',
                 'resources/js/auth.js',
+                'resources/js/owl.carousel.min.js'
             ],
             refresh: true,
         }),
