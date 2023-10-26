e2224c8199733c352fb0e6a37191437ba3bcae4b
valerio-m-alkimia <valerio.m@alkimiadigital.it>
2023-10-26 16:35:46 +0200

    add validation checkout

--- app/Livewire/Shop/Cart/Checkout.php
+++ app/Livewire/Shop/Cart/Checkout.php
@@ -11,15 +11,55 @@ use Illuminate\Support\Facades\Auth;
 class Checkout extends Component
 {
     public $email;
+    public $password;
+    public $emailGuest;
     public $privacy;
+    public $optionAccess;
+
+    public function rules() {
+        if ($this->optionAccess == 'login') {
+            return [
+                'email' => 'required',
+                'password' => 'required'
+            ];
+        }
+        if ($this->optionAccess == 'guest') {
+            return [
+                'emailGuest' => 'required',
+                'privacy' => 'required'
+            ];
+        }
+    }
+
+    public function messages() {
+        if ($this->optionAccess == 'login') {
+            return [
+                'email.required' => __('shop.payment.required'),
+                'password.required' => __('shop.payment.required'),
+            ];
+        }
+        if ($this->optionAccess == 'guest') {
+            return [
+                'emailGuest.required' => __('shop.payment.required'),
+                'privacy.required' => __('shop.payment.required'),
+            ];
+        }
+    }
 
     public function next() {
-        if ($this->email AND $this->privacy) {
+        $this->optionAccess = 'guest';
+
+        $this->validate();
+
+        if ($this->emailGuest AND $this->privacy) {
             $this->redirect('/shop/payment');
         }
     }
 
     public function login() {
+        $this->optionAccess = 'login';
+
+        $this->validate();
         $user = User::where('email', $this->email)->first();
 
         Auth::login($user);
--- resources/views/livewire/shop/cart/checkout.blade.php
+++ resources/views/livewire/shop/cart/checkout.blade.php
@@ -6,9 +6,10 @@
         </div>
 
         <form wire:submit="login" class="mb-[70px]">
-            <div class="w-full flex flex-col gap-1">
+            <div class="w-full flex flex-col gap-1 relative">
                 <label for="email" class="text-[12px] font-medium leading-[15px] text-color-18181a">{{ __('shop.checkout.email') }}</label>
                 <input wire:model="email" type="text" name="email" class="h-[48px] px-4 rounded-md bg-color-edebe5 p-0 border-color-e0e0df focus:border-transparent focus:ring-0">
+                <div class="absolute bottom-[-20px] right-0">@error('email') <span class="text-[12px] text-color-f4432c">{{ $message }}</span> @enderror</div>
             </div>
 
             <div x-data="{showPassword: false}" class="w-full flex flex-col gap-1 mt-[23px] mb-[40px] relative">
@@ -23,6 +24,7 @@
                         <x-icons name="eye-off-gray" class="w-4 h-4"></x-icons>
                     </template>
                 </div>
+                <div class="absolute bottom-[-20px] right-0">@error('password') <span class="text-[12px] text-color-f4432c">{{ $message }}</span> @enderror</div>
             </div>
 
             <button type="submit" class="relative flex items-center justify-center h-[48px] w-fit px-[40px] rounded-md bg-color-18181a border border-color-18181a group">
@@ -50,14 +52,16 @@
             <p class="text-[17px] leading-[20px] text-color-18181a font-normal">{{ __('shop.checkout.pay_to_guest') }}</p>
         </div>
 
-        <div class="w-full flex flex-col gap-1">
-            <label for="email" class="text-[12px] font-medium leading-[15px] text-color-18181a">{{ __('shop.checkout.email') }}</label>
-            <input wire:model="email" type="text" name="email" class="h-[48px] px-4 rounded-md bg-color-f2f0eb p-0 border-color-e0e0df focus:border-transparent focus:ring-0">
+        <div class="w-full flex flex-col gap-1 relative">
+            <label for="emailGuest" class="text-[12px] font-medium leading-[15px] text-color-18181a">{{ __('shop.checkout.email') }}</label>
+            <input wire:model="emailGuest" type="text" name="emailGuest" class="h-[48px] px-4 rounded-md bg-color-f2f0eb p-0 border-color-e0e0df focus:border-transparent focus:ring-0">
+            <div class="absolute bottom-[-20px] right-0">@error('emailGuest') <span class="text-[12px] text-color-f4432c">{{ $message }}</span> @enderror</div>
         </div>
 
-        <div class="flex items-start gap-1 mt-5 mb-[30px]">
+        <div class="flex items-start gap-1 mt-5 mb-[30px] relative">
             <x-checkbox-2 wire:model="privacy" :required="true" :name="'privacy'" />
             <p class="tex-[13px] leading-[18px] text-color-18181a">{{ __('shop.checkout.privacy')}}</p>
+            <div class="absolute bottom-[-20px] right-0">@error('privacy') <span class="text-[12px] text-color-f4432c">{{ $message }}</span> @enderror</div>
         </div>
 
         <button wire:click="next" class="relative flex items-center justify-center h-[48px] w-fit px-[40px] rounded-md bg-white border border-color-18181a group">
