dd0a512c7e1655973588cf60a0b3606f8678f849
alessandro-algomera <alessandro.visintini@algomera.it>
2023-11-07 18:37:22 +0100

    Impostati decremento e aumento quantità prodotti nei magazzini superadmin e reseller

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Country extends Model
 {
@@ -21,6 +22,11 @@ class Country extends Model
     public function users(): HasMany
     {
         return $this->hasMany(User::class);
+    }
+
+    public function reseller(): HasOne
+    {
+        return $this->hasOne(User::class)->role(User::RESELLER);
     }
 
     /**
