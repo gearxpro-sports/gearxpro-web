de1af876dba7d76b593cc0f4911204d995a345f0
fabio-algomera <fabio.serembe@algomera.it>
2023-11-08 09:35:45 +0100

    - Fix jumbotron.blade.php;

--- resources/views/livewire/shop/section/jumbotron.blade.php
+++ resources/views/livewire/shop/section/jumbotron.blade.php
@@ -15,7 +15,7 @@
 
         <div class="owl-carousel carousel_jumbotron">
             @foreach ($slides as $slide )
-                {{ Vite::asset('resources/images/gear/'.$slide)}}
+                <img src="{{ Vite::asset('resources/images/gear/'.$slide)}}" alt="">
             @endforeach
         </div>
 
