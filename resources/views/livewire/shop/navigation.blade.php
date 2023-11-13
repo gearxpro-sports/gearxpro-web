6a3304d84df0b8f057e85f9935b038526ea58805
fabio-algomera <fabio.serembe@algomera.it>
2023-11-13 11:33:44 +0100

    - Fix icona "account";

--- resources/views/livewire/shop/navigation.blade.php
+++ resources/views/livewire/shop/navigation.blade.php
@@ -89,7 +89,7 @@
                                 @else
                                     <a href="{{ route('login') }}"
                                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                                        <x-icons name="account"
+                                        <x-icons name="account" />
                                     </a>
                                 @endauth
                             </div>
