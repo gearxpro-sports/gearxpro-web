e02e73eafb6fa721515a41b879d6e6b138f54dc0
valerio-m-alkimia <valerio.m@alkimiadigital.it>
2023-11-14 23:33:07 +0100

    fix values

--- resources/views/livewire/about-us/values.blade.php
+++ resources/views/livewire/about-us/values.blade.php
@@ -1,10 +1,10 @@
 <div class="grid grid-cols-2 gap-x-7">
-    <div class="col-span-2 grid grid-cols-2 grid-rows-6 h-[984px] bg-about_us_values_1 relative">
+    <div class="col-span-2 grid grid-cols-2 grid-rows-6 h-[984px] bg-about_us_values_1 bg-fixed bg-cover bg-center bg-no-repeat relative">
         <div class="absolute top-0 left-0 w-full h-full bg-black/50"></div>
         <h1 class="z-10 col-span-1 row-start-3 pl-32 text-7xl font-bold text-white">I nostri valori sono chiari e presidiano il nostro lavoro.</h1>
     </div>
 
-    <div class="col-span-2 grid grid-cols-2 grid-rows-6 h-[1080px] bg-about_us_values_2 relative">
+    <div class="col-span-2 grid grid-cols-2 grid-rows-6 h-[1080px] bg-about_us_values_2 bg-fixed bg-cover bg-center bg-no-repeat relative">
         <div class="absolute top-0 left-0 w-full h-full bg-black/70"></div>
 
         <div class="col-span-1 row-start-3 row-span-3 flex flex-col justify-between">
@@ -30,7 +30,7 @@
         </div>
     </div>
 
-    <div class="col-span-2 grid grid-cols-2 grid-rows-6 h-[1080px] bg-about_us_values_3 relative">
+    <div class="col-span-2 grid grid-cols-2 grid-rows-6 h-[1080px] bg-about_us_values_3 bg-fixed bg-cover bg-center bg-no-repeat relative">
         <div class="absolute top-0 left-0 w-full h-full bg-black/50"></div>
         <h1 class="z-10 col-span-2 row-start-3 px-32 pt-24 text-6xl leading-[76px] font-bold text-white">
             L’unicità, l’integrità, l’ambiente ed il rispetto: sono questi i nostri valori chiari che ispirano la nostra cultura aziendale e che ci aiutano ogni giorno a mantenere alti standard etici, sociali e di qualità in ogni nostra attività.
