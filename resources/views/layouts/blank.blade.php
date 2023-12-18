81b0880b766a32280b1cb51f2a4267503a13ca6d
alessandro-algomera <alessandro.visintini@algomera.it>
2023-12-18 10:41:39 +0100

    Aggiunta favicon

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <x-favicon />
 	<title>
 		{{ $title ?? env('APP_NAME') }}
 	</title>
