9a8d8d69203f75751bf27a01da889c70caaf1656
fabio-algomera <fabio.serembe@algomera.it>
2023-12-20 17:02:38 +0100

    - Rimossa descrizione dal componente card-top.blade.php (categorie homepage);

--- resources/views/components/card-top.blade.php
+++ resources/views/components/card-top.blade.php
@@ -8,8 +8,8 @@
     <a href="{{ route('shop.index', ['country_code' => session('country_code'), 'categoryId' => $id_cat])}}"
         class="h-[61px] w-full bg-white flex border border-b-0 border-color-18181a rounded-b-md">
         <div class="grow flex flex-col justify-center px-[20px] relative">
-            <h3 class="z-10 text-[15px] font-semibold leading-[19px] group-hover:translate-y-[10px] transition-all duration-500">{{ $title }}</h3>
-            <p class="z-10 text-[12px] font-medium text-[#6C757D] group-hover:opacity-0 transition-all duration-500 ">{!! html_entity_decode($description) !!}</p>
+            <h3 class="z-10 text-[15px] font-semibold leading-[19px]">{{ $title }}</h3>
+{{--            <p class="z-10 text-[12px] font-medium text-[#6C757D] group-hover:opacity-0 transition-all duration-500 ">{!! html_entity_decode($description) !!}</p>--}}
             <div class="h-full absolute top-0 left-0 w-0 bg-color-f3f7f9 group-hover:animate-line group-hover:w-full rounded-bl-md"></div>
         </div>
         <div class="h-full w-16 flex items-center justify-center border-l border-color-18181a bg-color-f3f7f9">
