3afa5b51b18a4ca6dcb13f4501c410e7810adf25
fabio-algomera <fabio.serembe@algomera.it>
2023-12-20 09:58:04 +0100

    - Aggiunto servizio ip-api.com Pro; - Aggiunta chiave IP_API_KEY al file .env;

--- app/Services/IpApiService.php
+++ app/Services/IpApiService.php
@@ -7,19 +7,24 @@ use Illuminate\Support\Facades\Http;
 
 class IpApiService
 {
-    const BASE_PATH = 'http://ip-api.com/json/';
-
     /**
      * @var Collection|null
      */
     private ?Collection $ipInfo = null;
 
     /**
-     * @param string $ip
+     * @param  string  $ip
      */
     public function __construct(private string $ip)
     {
-        $this->ipInfo = rescue(fn () => Http::get(self::BASE_PATH.$this->ip)->collect());
+        if (env('IP_API_KEY')) {
+            $basePath = 'http://pro.ip-api.com/json/';
+            $url = $basePath.$this->ip."?key=".env('IP_API_KEY');
+        } else {
+            $basePath = 'http://ip-api.com/json/';
+            $url = $basePath.$this->ip;
+        }
+        $this->ipInfo = rescue(fn() => Http::get($url)->collect());
     }
 
     /**
@@ -31,7 +36,7 @@ class IpApiService
     }
 
     /**
-     * @param string ...$fields
+     * @param  string  ...$fields
      * @return array|string|null
      */
     public function getIpInfo(string ...$fields): array|string|null
