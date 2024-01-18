1815381cc1804984a7a937e974ed501df2cc917a
fabio-algomera <fabio.serembe@algomera.it>
2024-01-18 12:48:09 +0100

    - Inserimento tabelle in sizes-guide.blade.php (da file txt inviato da cliente);

--- resources/views/sizes-guide.blade.php
+++ resources/views/sizes-guide.blade.php
@@ -1,10 +1,234 @@
 <x-guest-layout>
+    <style>
+        table {
+            border: 1px solid #b3adad;
+            border-collapse: collapse;
+            padding: 5px;
+        }
+
+        table th {
+            border: 1px solid #b3adad;
+            padding: 5px;
+            background: #f0f0f0;
+            color: #313030;
+        }
+
+        table td {
+            border: 1px solid #b3adad;
+            text-align: center;
+            padding: 5px;
+            background: #ffffff;
+            color: #313030;
+        }
+    </style>
+
     <div class="bg-color-f2f0eb py-8 h-fit xl:px-10 xl:py-20">
         <div id="title" class="px-4 xl:px-0">
             <h1 class="text-3xl xl:text-7xl font-bold text-color-18181a">{{__('shop.sizes-guide.title')}}</h1>
         </div>
-        <div class="mt-4 px-4 xl:px-0">
-            Contenuto
+        <div class="mt-10 px-4 xl:px-0 space-y-8">
+            <div>
+                <h3 class="font-semibold mb-1.5">SOXPro Ultra Light e SOXPro Sprint</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>XS</th>
+                        <th>S</th>
+                        <th>M</th>
+                        <th>L</th>
+                        <th>XL</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>EUR 35-37</td>
+                        <td>&nbsp;EUR 38-40</td>
+                        <td><span style="font-style: normal; font-weight: 400;">EUR 41-43</span>&nbsp;</td>
+                        <td>&nbsp;EUR 44-46</td>
+                        <td><span style="font-style: normal; font-weight: 400;">EUR 47-49</span>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;UK 2.5-4.5</td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 5-7</span>&nbsp;</td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 7.5-9.5</span>&nbsp;</td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 10-12</span>&nbsp;</td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 11.5-13.5</span>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td><span style="font-style: normal; font-weight: 400;">US 3-5</span></td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 5.5-7.5</span>&nbsp;</td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 8-10</span></td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 10.5-11.5</span><span
+                                style="font-style: normal; font-weight: 400;">&nbsp;</span>&nbsp;
+                        </td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 12-14</span>&nbsp;</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div>
+                <h3 class="font-semibold mb-1.5">SOXPro Classic, SOXPro Trekking, SOXPro Fast Break, SOXPro Padel,
+                    SOXPro Ankle Support</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>S</th>
+                        <th>M</th>
+                        <th>L</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>&nbsp;EUR 35-40</td>
+                        <td><span style="font-style: normal; font-weight: 400;">EUR 41-46</span></td>
+                        <td>EUR 47-52</td>
+                    </tr>
+                    <tr>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 3-6.5</span></td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 7.5-11</span>&nbsp;</td>
+                        <td>&nbsp;UK 12-16.5</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;US 5-8.5</td>
+                        <td>&nbsp;US 8.5-12</td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 13-17</span></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div>
+                <h3 class="font-semibold mb-1.5">SOXPro Five To</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>S</th>
+                        <th>M</th>
+                        <th>L</th>
+                        <th>XL</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>&nbsp;EUR 36-38</td>
+                        <td><span style="font-style: normal; font-weight: 400;">EUR 39-41</span></td>
+                        <td>EUR 42-44</td>
+                        <td>EUR 45-47</td>
+                    </tr>
+                    <tr>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 3-5.5</span></td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 5.5-7</span>&nbsp;</td>
+                        <td>&nbsp;UK 8-9.5</td>
+                        <td>&nbsp;UK 10.5-12</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;US 4.5-6</td>
+                        <td>&nbsp;US 6.5-8</td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 9-10.5</span></td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 11.5-13</span></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div>
+                <h3 class="font-semibold mb-1.5">SOXPro Low Cut</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>S</th>
+                        <th>M</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>&nbsp;EUR 35-40</td>
+                        <td><span style="font-style: normal; font-weight: 400;">EUR 41-46</span></td>
+                    </tr>
+                    <tr>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 3-6.5</span></td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 7.5-11</span>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;US 5-8.5</td>
+                        <td>&nbsp;US 8.5-12</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div>
+                <h3 class="font-semibold mb-1.5">SOXPro Recovery</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>S</th>
+                        <th>M</th>
+                        <th>L</th>
+                        <th>XL</th>
+                        <th>XXL</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>&nbsp;EUR 37-39</td>
+                        <td>EUR 39-41&nbsp;</td>
+                        <td>EUR 41-43&nbsp;</td>
+                        <td><span style="font-style: normal; font-weight: 400;">EUR 43-46</span></td>
+                        <td>EUR 45-47</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;US 4-6</td>
+                        <td>US&nbsp; 5.5-7.5</td>
+                        <td>US 8-10&nbsp;</td>
+                        <td>US 10.5-11.5&nbsp;</td>
+                        <td>US 12-14&nbsp;</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+            <div>
+                <h3 class="font-semibold mb-1.5">SOXPro Compression</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>XS +-</th>
+                        <th>S +-</th>
+                        <th>M+-</th>
+                        <th>L +-</th>
+                        <th>XL +-</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>EUR 35-37</td>
+                        <td>&nbsp;EUR 38-40</td>
+                        <td><span style="font-style: normal; font-weight: 400;">EUR 41-43</span>&nbsp;</td>
+                        <td>&nbsp;EUR 44-46</td>
+                        <td><span style="font-style: normal; font-weight: 400;">EUR 47-49</span>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;UK 2.5-4.5</td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 5-7</span>&nbsp;</td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 7.5-9.5</span>&nbsp;</td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 10-12</span>&nbsp;</td>
+                        <td><span style="font-style: normal; font-weight: 400;">UK 11.5-13.5</span>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td><span style="font-style: normal; font-weight: 400;">US 3-5</span></td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 5.5-7.5</span>&nbsp;</td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 8-10</span></td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 10.5-11.5</span><span
+                                style="font-style: normal; font-weight: 400;">&nbsp;</span>&nbsp;
+                        </td>
+                        <td><span style="font-style: normal; font-weight: 400;">US 12-14</span>&nbsp;</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </x-guest-layout>
